4a02830a5ffc0bb2216911b1c03c765a5cc06e9f
QistiIzatus <76423044+QistiIzatus@users.noreply.github.com>
2022-10-21 18:56:49 +0700

    Menyelesaikan latihan vidio 4 laravek

--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,72 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view ('Home');
+    return view ('Home', [ 
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view ('About', [
+        "title" => "About",
         "name" => "Qisti Izatussyabani",
         "email" => "qistiizatussyabani8910@gmail.com",
         "image" => "qisti.jpg",
     ]);
 });
 
+
 Route::get('/blog', function () {
-    return view ('posts');
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Qisti Izatussyabani",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio error dignissimos ut sapiente non? Rem odio, sapiente tempora sed quaerat aspernatur? Asperiores, deserunt aliquam sunt obcaecati, veritatis quos dolore inventore rerum minima cumque cum. Vitae a nesciunt accusantium magni aliquam necessitatibus soluta? Hic laboriosam ab consectetur corporis corrupti mollitia asperiores neque quos dignissimos, odit dolorem suscipit ipsa unde animi excepturi autem totam adipisci! Reprehenderit veritatis possimus omnis quasi necessitatibus atque temporibus ratione adipisci, excepturi officiis facere nostrum blanditiis natus ad."
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Berliana Putri",
+            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem incidunt accusantium debitis distinctio, repellat praesentium sed, reprehenderit, maiores ab ullam saepe similique. Iusto totam explicabo dolor ducimus exercitationem, eaque fugiat placeat maiores animi voluptatum rem ab doloremque aliquam porro hic voluptas minima doloribus nisi soluta nulla. Nemo laborum similique molestiae nostrum ab placeat sed sequi tempora, eos, atque officiis corrupti minus modi vel voluptas qui quasi, a esse aut corporis incidunt quam rerum voluptate. Architecto delectus, perferendis odio voluptas voluptates repellendus amet eaque doloremque nulla laboriosam, tempore perspiciatis doloribus hic? Error numquam eum veniam hic illo quidem qui dolor quo!"
+        ],
+    ];
+    
+    return view ('posts', [
+        "title" => "Posts",
+        "posts" => $blog_posts
+    ]);
+});
+
+
+//Halaman single post
+
+Route::get('posts/{slug}', function($slug) {
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Qisti Izatussyabani",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio error dignissimos ut sapiente non? Rem odio, sapiente tempora sed quaerat aspernatur? Asperiores, deserunt aliquam sunt obcaecati, veritatis quos dolore inventore rerum minima cumque cum. Vitae a nesciunt accusantium magni aliquam necessitatibus soluta? Hic laboriosam ab consectetur corporis corrupti mollitia asperiores neque quos dignissimos, odit dolorem suscipit ipsa unde animi excepturi autem totam adipisci! Reprehenderit veritatis possimus omnis quasi necessitatibus atque temporibus ratione adipisci, excepturi officiis facere nostrum blanditiis natus ad."
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Berliana Putri",
+            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem incidunt accusantium debitis distinctio, repellat praesentium sed, reprehenderit, maiores ab ullam saepe similique. Iusto totam explicabo dolor ducimus exercitationem, eaque fugiat placeat maiores animi voluptatum rem ab doloremque aliquam porro hic voluptas minima doloribus nisi soluta nulla. Nemo laborum similique molestiae nostrum ab placeat sed sequi tempora, eos, atque officiis corrupti minus modi vel voluptas qui quasi, a esse aut corporis incidunt quam rerum voluptate. Architecto delectus, perferendis odio voluptas voluptates repellendus amet eaque doloremque nulla laboriosam, tempore perspiciatis doloribus hic? Error numquam eum veniam hic illo quidem qui dolor quo!"
+        ],
+    ];
+
+
+    $new_post = [];
+    foreach($blog_posts as $post){
+        if($post["slug"] == $slug) {
+            $new_post = $post;
+        }
+    }
+
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
+    ]);
 });
