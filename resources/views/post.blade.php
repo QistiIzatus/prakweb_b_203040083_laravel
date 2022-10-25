9bc231c86e092ed301df5127675d9e31bc29f900
QistiIzatus <76423044+QistiIzatus@users.noreply.github.com>
2022-10-25 17:50:41 +0700

    Menyelesaikan latihan vidio ke 8 Post Category & Eloquent Relationship

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,6 +4,8 @@
 
 
         <h2>{{ $post->title }}</h2>
+
+        <p>By. Qisti Izatussyabani in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
         
         {!! $post->body !!}
     
