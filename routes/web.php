8aa9b1e3c22f53528a04f47139e5d5742f4f56c7
QistiIzatus <76423044+QistiIzatus@users.noreply.github.com>
2022-11-05 19:45:10 +0700

    Menyelesaikan latihan vidio 15 User Registration

--- routes/web.php
+++ routes/web.php
@@ -54,6 +54,7 @@ Route::get('/categories', function() {
 Route::get('/login', [LoginController::class, 'index']);
 
 Route::get('/register', [RegisterController::class, 'index']);
+Route::post('/register', [RegisterController::class, 'store']);
 
 
 
